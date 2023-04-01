5de4170c2f724f7a2a020be8f76a224b2eee381b
DmitryRyzhik <rdssdr03@gmail.com>
2023-04-02 01:44:42 +0800

    Итоговый проект Домашнее задание

---
+++ 1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+int ReadInt(string prompt)
+{
+    System.Console.Write(prompt);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void NaturalToLow(int n, int count)
+{
+    if (count > n)
+    {
+        return;
+    }
+    else
+    {
+        NaturalToLow(n, count + 1);
+        Console.Write(count + " ");
+    }
+}
+
+int number = ReadInt("Введите неотрицательное число m: ");
+int count = 1;
+NaturalToLow(number, count);
